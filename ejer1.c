0b509cf453a67e896c610665fc3fce380abb6635
ArmandoVVV <73661094+ArmandoVVV@users.noreply.github.com>
2022-01-31 16:52:13 -0700

    Add files via upload

---
+++ ejer1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(){
+    int p;
+    int i = 10;
+    p = fork();
+    if(p == 0){
+        while(i != 0){
+            sleep(2);
+            printf("soy el hijo\n");
+            i--;
+        }
+    }
+    else{
+        sleep(1);
+        while( i != 0){
+            printf("soy el padre\n");
+            i--;
+            sleep(2);
+        }
+        wait(NULL);
+        printf("Mi proceso hijo ya ha terminado\n");
+    }
+    
+    return 0;
+}
