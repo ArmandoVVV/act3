f0c6896b246bdd950328bc1ec989f89469c15fe3
ArmandoVVV <73661094+ArmandoVVV@users.noreply.github.com>
2022-02-01 19:54:01 -0700

    Add files via upload

---
+++ ejer5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <signal.h>
+
+//ejemplo de input: ./ejer1.out
+
+int main(){
+    char comando[30];
+	int pid;
+	int salida = 0;
+	
+	do{
+		printf(">");
+		fgets(comando,  80, stdin);
+		strtok(comando, "\n");
+		if(strcmp(comando, "exit") == 0)
+			salida = 1;
+		else{
+            pid = fork();
+            if(pid == 0){
+                execlp(comando, comando, NULL);
+            }
+            else{
+                wait(NULL);
+                }
+        }         
+		memset(comando, 0, sizeof(comando));
+    }while(!salida);
+}
